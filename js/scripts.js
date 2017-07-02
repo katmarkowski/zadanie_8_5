84146531bf9a9ae76b5a2b8365bac0b6235995e3
Krzysztof Markowski <markowski@gdansk.home.pl>
2017-07-02 17:28:00 +0200

    Added Triangel area in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
- 
+var a = prompt('Podaj długość podstawy trójkąta (tylko liczba - bez jednostek!):'),
+	h = prompt('Podaj wysokość (tylko liczba - bez jednostek!):'),
+	triangleArea;
+triangleArea = a * h / 2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
